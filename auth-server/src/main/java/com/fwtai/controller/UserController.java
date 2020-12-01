8681b75ef6dbfc6064a3fbeb4808319eed3e7c81
gzstyp <444141300@qq.com>
2020-12-01 10:11:35 +0800

    配置获取用户信息的Controller,用于认证成功后获取登录者的信息,这个方便各个子系统获取登录信息

--- auth-server/src/main/java/com/fwtai/controller/UserController.java
+++ auth-server/src/main/java/com/fwtai/controller/UserController.java
@@ -1,15 +1,17 @@
 package com.fwtai.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
-    //认证成功后获取登录者的信息
-    @RequestMapping("/user/me")
+    //认证成功后获取登录者的信息,这个方便各个子系统获取登录信息
+    @GetMapping("/info")
     public Principal user(final Principal principal) {
         System.out.println(principal);
         return principal;
