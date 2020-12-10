352063e6297322f1b4f680feff045a8f4018c6eb
gzstyp <444141300@qq.com>
2020-12-10 12:44:14 +0800

    证成功后获取登录者的信息

--- client-coupon/src/main/java/com/fwtai/controller/CouponController.java
+++ client-coupon/src/main/java/com/fwtai/controller/CouponController.java
@@ -2,6 +2,7 @@ package com.fwtai.controller;
 
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,10 +17,16 @@ public class CouponController {
      * 会员列表页面,http://192.168.3.108:8082/coupon/list
     */
     @RequestMapping("/list")
-    public ModelAndView list(final Principal principal){
-        System.out.println(principal);
+    public ModelAndView list(final Principal pl){
+        System.out.println(pl);
         final SecurityContext context = SecurityContextHolder.getContext();//获取当前登录账号信息
         System.out.println(context);
+        final Object principal = context.getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails)principal).getUsername();
+        } else {
+            String username = principal.toString();
+        }
         return new ModelAndView("coupon/list");
     }
 }
