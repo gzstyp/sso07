fad4dcc98fe02dbcc677a429d166b0f5f2d94b10
gzstyp <444141300@qq.com>
2020-12-10 12:43:29 +0800

    证成功后获取登录者的信息

--- auth-server/src/main/java/com/fwtai/controller/UserController.java
+++ auth-server/src/main/java/com/fwtai/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.fwtai.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,14 @@ public class UserController {
 
     //认证成功后获取登录者的信息,这个方便各个子系统获取登录信息
     @GetMapping("/info")
-    public Principal user(final Principal principal) {
-        System.out.println(principal);
-        return principal;
+    public Principal user(final Principal pl) {
+        System.out.println(pl);
+        final Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            String username = ((UserDetails)principal).getUsername();
+        } else {
+            String username = principal.toString();
+        }
+        return pl;
     }
 }
